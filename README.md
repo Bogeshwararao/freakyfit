391043f108e71217d9868ca95b8eabca5e804af4
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-10-06 16:34:36 +0630

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-ReactJs Dashboard UI
--- src/Context/NavContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const NavContext = createContext({
-  nav: false,
-  setNav: () => {},
-});
-export default NavContext;
