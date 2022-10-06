ae7e626dc513de5380760702f559dc5e14ce5077
bogeshwararao <bogeshwararao12042004@gmail.com>
2022-10-06 16:36:05 +0630

    updated

---
+++ lib/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+const Color white = Color.fromARGB(255, 250, 245, 245);
+const Color black = Color(0xFF000000);
+
+const Color primary = Color(0xffFFDD00);
+const Color secondary = Color(0xffFFDD00);
+const Color thirdColor = Color(0xffFFDD00);
+const Color fourthColor = Color(0xffFFDD00);
+const Color bgTextField = Color(0xffFFDD00);
+
+const Color facebookColor = Color(0xffFFDD00);
+const Color googleColor = Color(0xFFDB4437);
